fad9a09523e0831942a2263d6b6ab5b9bad6eaff
solidtop <solidtop.asp@gmail.com>
2025-03-26 19:24:39 +0100

    Chat only available when authenticated

--- chatapp.client/src/app/features/home/home-page/home-page.component.ts
+++ chatapp.client/src/app/features/home/home-page/home-page.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { MainHeaderComponent } from '../../../shared/components/main-header/main-header.component';
+import { AuthService } from '../../auth/services/auth.service';
 import { ChatComponent } from '../../chat/components/chat/chat.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [MainHeaderComponent, ChatComponent],
+  imports: [MainHeaderComponent, ChatComponent, AsyncPipe],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
-  
+  readonly authService = inject(AuthService);
 }
