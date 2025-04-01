b77924f98d5feb5d7728472567f1b75a7a9e0a79
solidtop <solidtop.asp@gmail.com>
2025-04-01 10:34:31 +0200

    Setup account page

--- chatapp.client/src/app/app.routing.ts
+++ chatapp.client/src/app/app.routing.ts
@@ -1,4 +1,6 @@
 import { Routes } from "@angular/router";
+import { AccountPageComponent } from "./features/account/pages/account-page/account-page.component";
+import { authGuard } from "./features/auth/guards/auth.guard";
 import { LoginPageComponent } from "./features/auth/pages/login-page/login-page.component";
 import { RegisterPageComponent } from "./features/auth/pages/register-page/register-page.component";
 import { HomePageComponent } from "./features/home/home-page/home-page.component";
@@ -7,4 +9,5 @@ export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'register', component: RegisterPageComponent },
   { path: 'login', component: LoginPageComponent },
+  { path: 'account', component: AccountPageComponent, canActivate: [authGuard] },
 ];
